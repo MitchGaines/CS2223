a438b9fc86260cdfc01caa86f1f23e5576eaba0f
Mitch Gaines <gainesmitch2@gmail.com>
2017-04-14 15:00:30 -0400

    added header comments to python code

--- prj1/proj1.py
+++ prj1/proj1.py
@@ -1,21 +1,17 @@
+'Jonathan Gaines'
+'CS2223 - Algorithms'
+'Project 1'
+'Description: This program analyzes and compares the time efficiencies of '
+'a divide and conquer algorithm, quicksort and a brute force algorithm, '
+'bubble sort.'
+
+
 import time
 import sys
 from random import *
 
-sm_min = 1
-sm_max = 1000
-md_min = sm_max+1
-md_max = 15000
-lg_min = md_max+1
-lg_max = 100000
-
-print("----------------------------------------")
-print("CS2223 Project 1 | Jonathan Gaines")
-print("\n1 - Small Set (" + str(sm_min)+ " - " + str(sm_max) + ")")
-print("2 - Medium Set (" + str(md_min) + " - " + str(md_max) + ")")  
-print("3 - Large Set (" + str(lg_min) + " - " + str(lg_max) + ")")
-
 def bubblesort(alist):
+    'sorts a given list using bubble sort and returns a sorted list'
     for passnum in range(len(alist)-1, 0, -1):
         for i in range(passnum):
             if alist[i] > alist[i+1]:
@@ -25,6 +21,7 @@ def bubblesort(alist):
     return alist 
 
 def partition(alist, begin, end):
+    'used with quicksort func and returns new pivot points'
     pivot = begin
     for i in range(begin+1, end+1):
         if alist[i] <= alist[begin]:
@@ -35,12 +32,14 @@ def partition(alist, begin, end):
     return pivot
 
 def quicksort(alist, begin=0, end=None):
+    'takes a list and sorts it using quicksort'
     if end is None:
         end = len(alist) - 1
     def _quicksort(alist, begin, end):
         if begin >= end:
             return 
         pivot = partition(alist, begin, end)
+        'recursively call quicksort on divided up list'
         _quicksort(alist, begin, pivot-1)
         _quicksort(alist, pivot+1, end)
         return alist
@@ -48,6 +47,9 @@ def quicksort(alist, begin=0, end=None):
     return _quicksort(alist, begin, end)
 
 def gen_rand_list(length):
+    'generates a list of given length with randomly assigned integers'
+    'not exceeding sys.maxsize/4'
+
     print("\nConstructing Dataset . . .")
     print("Dataset Size: " + str(length))
     x = []
@@ -59,6 +61,8 @@ def gen_rand_list(length):
     return x
 
 def effDC(dataset):
+    'finds time efficiency of divide and conquer algorithm and' 
+    'prints first and last values'
     start = time.time()
     alist = quicksort(dataset)
     end = time.time()
@@ -68,6 +72,8 @@ def effDC(dataset):
 
 
 def effBF(dataset):
+    'finds time efficiency of brute force algorithm and'
+    'prints first and last values'
     start = time.time()
     alist = bubblesort(dataset)
     end = time.time()
@@ -77,6 +83,8 @@ def effBF(dataset):
 
 
 def run_algs(setVal):
+    'run algorithms with small, medium, or large input'
+    'size based on user input'
     if int(setVal) == 1:
         sm_val = randint(sm_min, sm_max)
         alist = gen_rand_list(sm_val)
@@ -90,6 +98,25 @@ def run_algs(setVal):
     effDC(alist)
     effBF(alist)
     print("***********************************")
+
+
+'Initial program'
+
+'Dataset size values, min & max'
+sm_min = 1
+sm_max = 1000
+md_min = sm_max+1
+md_max = 15000
+lg_min = md_max+1
+lg_max = 100000
+
+print("----------------------------------------")
+print("CS2223 Project 1 | Jonathan Gaines")
+print("\n1 - Small Set (" + str(sm_min)+ " - " + str(sm_max) + ")")
+print("2 - Medium Set (" + str(md_min) + " - " + str(md_max) + ")")  
+print("3 - Large Set (" + str(lg_min) + " - " + str(lg_max) + ")")
+
+
 
 setSize = 0
 while int(setSize) != -1:
