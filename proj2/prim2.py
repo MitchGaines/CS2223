4e1516fb2d0dea9a932ca03f5f94e15fb375bafd
Mitch Gaines <gainesmitch2@gmail.com>
2017-04-28 11:58:00 -0400

    prims fixed brother node nonsense

--- proj2/prim2.py
+++ proj2/prim2.py
@@ -8,7 +8,8 @@ def val_exist(element, ls):
     else:
         return 0
 
-def rm_rows(ls, vals):
+#returns a 2d list without rows 
+def rm_rows_with_vals(ls, vals):
     temp = []
     for i in range(0, len(ls)):
         if not(val_exist(ls[i][1], vals) or val_exist(ls[i][2], vals)):
@@ -16,15 +17,22 @@ def rm_rows(ls, vals):
 
     return temp
 
-def rm_row(ls, index):
-    ls.pop(index)
-    return ls
+#returns a 2d list without rows of given index values
+def rm_rows_with_indexes(ls, indexes):
+    temp = []
+    for i in range(0, len(ls)):
+        if i not in indexes:
+            temp.append(ls[i])
 
+    return temp
+
+#gets the index of an edge from the edge list
 def get_edges_index(edges, ls):
     for i in range(0, len(edges)):
         if set(edges[i]) == set(ls):
             return i
 
+#creates a list of all the nodes in the list of edges
 def get_unique_nodes(edges):
     vals = []
     for i in edges:
@@ -34,6 +42,7 @@ def get_unique_nodes(edges):
     vals = set(vals)
     return vals
 
+#implements prim's algorithm
 def prim(edges):
     mst = []
     active_list = []
@@ -42,18 +51,14 @@ def prim(edges):
     mod_edges = edges
     active_list_new = active_list
 
-    while (len(curr_nodes) < len(all_nodes)-1):
-       # print("Edges: " + str(edges))
-       # print("Active List: " + str(active_list)) 
+    while (len(curr_nodes) <= len(all_nodes)+2):
         #search edges for selected nodes and add to active list
         for i in range(0, len(edges)):
             for j in range(0, len(curr_nodes)):
-                if edges[i][1] == curr_nodes[j] or edges[i][2] == curr_nodes[j]:
+                if (edges[i][1] == curr_nodes[j] and edges[i][2] not in curr_nodes) or (edges[i][2] == curr_nodes[j] and edges[i][1] not in curr_nodes):
                     active_list.append(edges[i])
-                    #print(active_list)
-                    #print("\n\n-------- \n\n")
 
-        mod_edges = rm_rows(mod_edges, curr_nodes)
+        mod_edges = rm_rows_with_vals(mod_edges, curr_nodes)
 
         curr_min = float("inf")
         curr_min_index = -1
@@ -61,19 +66,23 @@ def prim(edges):
         for i in range(0, len(active_list)):
             if curr_min > active_list[i][0]:
                 curr_min = active_list[i][0]
-
+        
         #look for curr_min's row and add that to mst and its brother node to curr_nodes
         for i in range(0, len(active_list)):
             if curr_min == active_list[i][0]:
                 curr_min_index = i
-                mst.append(active_list[i])
+                #only add if and only if one element is in curr_nodes and one element is not
+                if (active_list[i][1] not in curr_nodes) ^ (active_list[i][2] not in curr_nodes):
+                    mst.append(active_list[i])
+                added_node = -1
                 if active_list[i][1] in curr_nodes:
-                    curr_nodes.append(active_list[i][2]) # NOT WORKING HERE, NOT ADDING PARTNER NODES
+                    added_node = active_list[i][2]
+                    curr_nodes.append(added_node)  
                 elif active_list[i][2] in curr_nodes:
-                    curr_nodes.append(active_list[i][1])
-                #edges = rm_row(edges, get_edges_index(edges, active_list[i]))
-        
-        active_list_new = rm_row(active_list, curr_min_index) 
+                    added_node = active_list[i][1]
+                    curr_nodes.append(added_node)
+
+        active_list_new = rm_rows_with_indexes(active_list, [curr_min_index])
         edges = mod_edges
         active_list = active_list_new
     
